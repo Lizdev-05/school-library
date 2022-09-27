fcb9c0f63467724caa4d619ff28e297ef86c7306
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-27 14:45:16 +0100

    Add initializer

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
-      
-
+    def initialize(name = 'Unknown', age, parent_permission = true)
+        @id = Random.rand(1..100)
+        @name = name
+        @age = age 
+    end
 end
