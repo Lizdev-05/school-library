b7ac0c8f888d650d1c5a22ea3c055ac5ddf9a779
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-28 09:54:18 +0100

    make nameable the superclass of person

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, parent_permission: true, name: 'Unknown')
-   
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
