9445b74dfcb16ed6edad05254e026ade9844ff62
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-29 09:01:12 +0100

    update person class

---
+++ main.rb
@@ -0,0 +1,16 @@
+puts 'Welcome to School Library App!'
+puts
+puts 'Please choose an option by entering a number: '
+1 - 'List all books'
+2 - 'List all people'
+3 - 'Create  a person'
+4 - 'Create  a book'
+5 - 'Create  a rental'
+6 - 'List all rentals for a given person id'
+7 - 'Exist'
+ 
+option = gets.chomp.to_i
+
+case prompt
+    When '1'
+     
