07efe806df6096481d3e1b95b11d420719107d71
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-29 09:34:18 +0100

    fix linters

---
+++ app.rb
@@ -0,0 +1,3 @@
+def shout
+  '-------'
+end
--- main.rb
+++ main.rb
@@ -1,16 +1,14 @@
 puts 'Welcome to School Library App!'
 puts
-puts 'Please choose an option by entering a number: '
-1 - 'List all books'
-2 - 'List all people'
-3 - 'Create  a person'
-4 - 'Create  a book'
-5 - 'Create  a rental'
-6 - 'List all rentals for a given person id'
-7 - 'Exist'
- 
-option = gets.chomp.to_i
-
-case prompt
-    When '1'
-     
+def display_list
+  puts 'Welcome to School library App!'
+  puts "
+      Choose an option by entering a number:
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit"
+end
