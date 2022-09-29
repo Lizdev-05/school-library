aeb104132debb52797487991508d4c1920933666
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-29 10:15:58 +0100

    update class app

--- main.rb
+++ main.rb
@@ -1,14 +1,19 @@
-puts 'Welcome to School Library App!'
-puts
-def display_lists
-  puts 'Welcome to School library App!'
-  puts "
-      Choose an option by entering a number:
-      1 - List all books
-      2 - List all people
-      3 - Create a person
-      4 - Create a book
-      5 - Create a rental
-      6 - List all rentals for a given person id
-      7 - Exit"
-end
+class Main
+    def self.home_page
+      puts 'Please enter a number: '
+  
+      @content = {
+        '1' => 'List all books',
+        '2' => 'List all people',
+        '3' => 'Create a person (teacher or student)',
+        '4' => 'Create a book',
+        '5' => 'Create a rental',
+        '6' => 'List all rentals for a given person id',
+        '7' => 'Exit'
+      }
+      @content.each { |index, string| puts "#{index} - #{string}" }
+  
+      Integer(gets.chomp)
+    end
+
+
