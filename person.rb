81d14a8b2e841e314a3c66fe609b286568ed6c3e
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-04 17:18:55 +0100

    refactor the code app file to read files

--- person.rb
+++ person.rb
@@ -1,33 +1,71 @@
 require_relative 'nameable'
 require_relative 'rental'
-class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
 
-  def initialize(age, name, parent_permission: true)
+class Person < Nameable
+  attr_accessor :name, :age, :parent_permission, :rentals
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
-    @id = Random.rand(1..100)
+
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
 
-  def of_age
-    @age >= 18
-  end
-
-  private :of_age
-
-  def can_use_service
-    of_age || @parent_permission
+  def can_use_services?
+    of_age? || parent_permission
   end
 
   def correct_name
-    @name
+    name
   end
 
-  def rent_book(book, date)
-    Rental.new(date, book, self)
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def to_s
+    index = Person.all.index(self)
+    "#{index})"
+  end
+
+  def of_age?
+    age >= 18
+  end
+
+  def self.list_people
+    puts 'List of people:'
+    puts all.empty? ? 'No people available yet' : all
+  end
+
+  def self.create_person
+    puts
+    puts 'Do you want to create a student (1) or a Teacher (2)? [Input the number]'
+    option = gets.chomp
+
+    case option
+    when '1'
+      Student.create_student
+    when '2'
+      Teacher.create_teacher
+    else
+      puts
+      puts 'That is not a valid input'
+    end
+  end
+
+  def self.select_person_to_rent
+    puts 'Select a person from the following list by number (not id)'
+    puts all
+    person = gets.chomp.to_i
+
+    if person > all.length || person.negative?
+      puts 'Invalid person number'
+      return
+    end
+    person
   end
 end
