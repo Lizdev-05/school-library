89eac9c6397c891eb9927196164c62a11b9ee09a
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-05 12:12:59 +0100

    add test for the name, age and specialization

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -11,6 +11,18 @@ describe Teacher do
             expect(@teacher).to be_an_instance_of(Teacher)
         end
     
-       
+        it 'Should return the correct name' do
+            expect(@teacher.name).to eq('John')
+        end
+    
+        it 'Should return the correct age' do
+            expect(@teacher.age).to eq(21)
+        end
+    
+        it 'Should return the correct permission' do
+            expect(@teacher.parent_permission).to eq(true)
+        end
+        end
+    end
     
     end
