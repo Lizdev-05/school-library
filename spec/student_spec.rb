a798285e4db021d4cc48344702c0bf3820a3ba9f
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-05 12:04:58 +0100

    Add test for instance of student

---
+++ spec/student_spec.rb
@@ -0,0 +1,15 @@
+require_relative ('spec_helper')
+
+describe Student do
+    before(:each) do
+        @student = Student.new(nil, age = 21, name = 'John', parent_permission: true)
+    end
+    
+    describe '#new' do
+        context 'When creating a new student' do
+        it 'Should return a Student object' do
+            expect(@student).to be_an_instance_of(Student)
+        end
+    
+      
+    end
