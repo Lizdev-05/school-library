ec15189d9451f7e4d6abeb83315ac90ba72f81e2
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-05 12:19:08 +0100

    Test the instance of classroom, label, new student, and student

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,26 @@
+require_relative ('spec_helper')
+
+describe Classroom do
+    before :each do
+        @classroom = Classroom.new('Math')
+    end
+    
+    describe '#new' do
+        it 'takes two parameters and returns a Classroom object' do
+        expect(@classroom).to be_an_instance_of Classroom
+        end
+    end
+    
+    describe '#label' do
+        it 'returns the correct label' do
+        expect(@classroom.label).to eql 'Math'
+        end
+    end
+    
+    
+    describe '#students' do
+        it 'returns an empty array when no students are added' do
+        expect(@classroom.student).to eql []
+        end
+    end
+end
